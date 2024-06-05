fd0ed0d155b6eda37ef07cc85cb016637b355319
rstocker99 <rstocker99@gmail.com>
2024-06-05 11:11:50 -0700

    Make version number compatible with pip 24.1

--- intercom/__init__.py
+++ intercom/__init__.py
@@ -7,7 +7,7 @@ from .errors import (ArgumentError, AuthenticationError, # noqa
     ServerError, ServiceUnavailableError, UnexpectedError, TokenUnauthorizedError)
 
 # Was __version__ = '3.1.0.RYAN' but pip doens't like that
-__version__ = '3.1.0.RYAN'
+__version__ = '3.1.1.1'
 
 
 RELATED_DOCS_TEXT = "See https://github.com/jkeyes/python-intercom \
